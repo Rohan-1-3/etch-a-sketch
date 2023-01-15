b580a2c9d6faef87ac5ed73febf4b3924b74e9fd
Rohan-1-3 <rohanshrestha130@gmail.com>
2023-01-15 18:23:04 +0545

    minor changes

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,71 +1,71 @@
-//GridSize Options
-var selectGridSize = document.getElementById("selectGridSize");
+// GridSize Options
+const selectGridSize = document.getElementById("selectGridSize");
 for (let i = 64; i >= 1; i--){
-  selectGridSize.innerHTML += "<option>" + i + "</option>";
+  selectGridSize.innerHTML += `<option>${  i  }</option>`;
 }
 
-//Making Grids Div
+// Making Grids Div
 const container = document.getElementById("container");
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (let c = 0; c < (rows * cols); c=+1) {
+    const cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item"
   };
 };
 
-//Initial Grid size
+// Initial Grid size
 makeRows(64,64)
 colorChange();
 
-//GridSize Accr to Users Choice
+// GridSize Accr to Users Choice
 function gridSize(){
-  document.getElementById('container').textContent = '';//removes previous grid size
-  const gridSize = document.querySelector('select').value;
+  document.getElementById("container").textContent = "";// removes previous grid size
+  const gridSize = document.querySelector("select").value;
   makeRows(gridSize, gridSize);
   colorChange();
 }
 
-//Grid Change color to black
+// Grid Change color to black
 function colorChange(){
-  const gridItems = document.querySelectorAll('.grid-item');
+  const gridItems = document.querySelectorAll(".grid-item");
   gridItems.forEach((gridItem)=>{
-    gridItem.addEventListener('mouseover', ()=>{
-      gridItem.style.backgroundColor = 'black';
+    gridItem.addEventListener("mouseover", ()=>{
+      gridItem.style.backgroundColor = "black";
     })
   })
 }
 
-//Adding Eraser
+// Adding Eraser
 function eraser(){
-  const gridItems = document.querySelectorAll('.grid-item');
+  const gridItems = document.querySelectorAll(".grid-item");
   gridItems.forEach((gridItem)=>{
-    gridItem.addEventListener('mouseover', ()=>{
-      gridItem.style.backgroundColor = 'white';
+    gridItem.addEventListener("mouseover", ()=>{
+      gridItem.style.backgroundColor = "white";
     })
   })
 }
 
-//Grid Change into random color
+// Grid Change into random color
 function rainbowColorChange(){
   function rgbRandom(){
     return Math.floor((Math.random()*255)+1);
   }
-  const gridItems = document.querySelectorAll('.grid-item');
+  const gridItems = document.querySelectorAll(".grid-item");
   gridItems.forEach((gridItem)=>{
-    gridItem.addEventListener('mouseover', ()=>{
+    gridItem.addEventListener("mouseover", ()=>{
       const rgbValue1= rgbRandom();
       const rgbValue2 = rgbRandom();
       const rgbValue3 = rgbRandom();
-      const finalRGB = `rgb(${rgbValue1},${rgbValue2},${rgbValue3})`//gets randome rgb value
+      const finalRGB = `rgb(${rgbValue1},${rgbValue2},${rgbValue3})`// gets randome rgb value
       gridItem.style.backgroundColor = finalRGB;
     })
   })
 
 }
 
-//Remove the sketch in current grid
+// Remove the sketch in current grid
 function resetGrid(){
   gridSize();
 }
